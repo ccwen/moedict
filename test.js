88b7bdab760245a31570a3fb03aeec9e4abf031a
yapcheahshen <yapcheahshen@gmail.com>
2015-01-05 14:16:11 +0800

    fix test

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
-var kde=require("ksana-document").kde;
-var kse=require("ksana-document").kse;
+var kde=require("ksana-database");
+var kse=require("ksana-search");
 var plist=require("ksana-document").plist;
-var test=function(engine) {
+var test=function(err,engine) {
 	var pageNames=engine.get("pageNames");
-	var i=plist.indexOfSorted(pageNames,"冰");
+	var i=plist.indexOfSorted(pageNames,"英");
 	engine.get(["fileContents",0,i],function(data){
 		console.log(data)
 	})
 }
-var test2=function(engine){
-	kse.search(engine,"下棋",{range:{start:0},nohighlight:true},function(data){
+
+var test2=function(){
+	kse.search("moedict","下棋",{range:{start:0},nohighlight:true},function(err,data){
 		console.log(data.excerpt)
 	});
 
 }
-kde.open("moedict",test2);
+test2();
+//kde.open("moedict",test2);
